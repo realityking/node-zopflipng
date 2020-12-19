373081b073cf37f6602d3d2259a422f23ede3c8e
Rouven Weßling <rouven@contentful.com>
2020-12-19 12:31:22 +0100

    Add first tests

---
+++ test/binding.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const {promisify} = require('util');
+
+const test = require('ava');
+const isPng = require('is-png');
+
+const zopflipng = require('..');
+
+const readFile = promisify(fs.readFile);
+
+test('optimize a PNG', async t => {
+	const buf = await readFile(path.join(__dirname, 'fixtures/test.png'));
+	const data = zopflipng(buf);
+
+	t.true(data.length < buf.length);
+	t.true(isPng(data));
+});
